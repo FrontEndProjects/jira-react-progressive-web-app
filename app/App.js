88b5f3005fccc3af147f9afff655a4a570c908b2
Filip Bialek <bialek.f@gmail.com>
2016-10-13 22:36:35 +0200

    webpack config

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h1>Header 1</h1>
+      </div>
+     );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+  devtool: 'eval-source-map',
+  entry: path.resolve(__dirname, 'app/App.js'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+  node: {
+    __dirname: true,
+    fs: 'empty'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public/'),
+    colors: true,
+    inline: true
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ];
+}
+
+module.exports = config;
