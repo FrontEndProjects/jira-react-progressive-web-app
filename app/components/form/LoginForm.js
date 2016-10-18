3497163cf5d37342f04084400be048c371012408
Filip Bialek <bialek.f@gmail.com>
2016-10-19 00:14:19 +0200

    more state in form

--- app/components/form/LoginForm.js
+++ app/components/form/LoginForm.js
@@ -8,19 +8,37 @@ class LoginForm extends Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      login: '',
+      password: ''
+    };
     this.handleLoginButton = this.handleLoginButton.bind(this);
+    this.handleLoginInput = this.handleLoginInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
   }
 
   handleLoginButton () {
-    console.log('Username: ', this.refs.username.getValue());
-    console.log('Password: ', this.refs.password.getValue());
+    console.log('Username: ', this.state.login);
+    console.log('Password: ', this.state.password);
+  }
+
+  handleLoginInput (e) {
+    this.setState({
+      login: e.target.value
+    });
+  }
+
+  handlePasswordInput (e) {
+    this.setState({
+      password: e.target.value
+    });
   }
 
   render () {
     return (
       <div className="input-form">
-        <TextField hintText="Enter your e-mail" fullWidth={true} ref="username" />
-        <TextField hintText="Enter your password" type="password" fullWidth={true} ref="password" />
+        <TextField hintText="Enter your login" fullWidth={true} value={this.state.login} onChange={this.handleLoginInput} />
+        <TextField hintText="Enter your password" type="password" fullWidth={true} value={this.state.password} onChange={this.handlePasswordInput} />
         <RaisedButton label="Log In" fullWidth={true} onClick={this.handleLoginButton}/>
       </div>
      );
