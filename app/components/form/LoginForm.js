7af05d9755fa7e7b2c79c1df2396f16d0f6c830c
irekrog <irekfrontend@gmail.com>
2016-10-18 22:02:34 +0200

    add refs to inputs

--- app/components/form/LoginForm.js
+++ app/components/form/LoginForm.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
-import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import './login-form.scss';
 
 class LoginForm extends Component {
+
+  constructor (props) {
+    super(props);
+    this.handleLoginButton = this.handleLoginButton.bind(this);
+  }
+
+  handleLoginButton () {
+    console.log('Username: ', this.refs.username.getValue());
+    console.log('Password: ', this.refs.password.getValue());
+  }
 
   render () {
     return (
       <div className="input-form">
         <TextField hintText="Enter your e-mail" fullWidth={true} ref="username" />
         <TextField hintText="Enter your password" type="password" fullWidth={true} ref="password" />
-        <FlatButton label="Log In" />
+        <RaisedButton label="Log In" fullWidth={true} onClick={this.handleLoginButton}/>
       </div>
      );
   }
