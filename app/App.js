bde4a8eaadc5372b750fd42f6da7f17cb55e1806
irekrog <irekfrontend@gmail.com>
2016-10-18 08:00:22 +0200

    add material ui components

--- app/App.js
+++ app/App.js
@@ -4,17 +4,41 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Card from './components/Card.js';
-import Header from './components/Header.js';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 
 injectTapEventPlugin();
 
 class App extends Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {open: false};
+    this.openDrawer = this.openDrawer.bind(this);
+  }
+
+  openDrawer () {
+    this.setState({open: !this.state.open});
+  }
+
   render () {
     return (
       <MuiThemeProvider>
         <div>
-          <Header />
+          <Drawer open={this.state.open} docked={false} onRequestChange={(open) => this.setState({open})}>
+            <Menu desktop={true}>
+              <MenuItem primaryText="Name of user" />
+              <Divider />
+              <MenuItem primaryText="Settings" />
+              <MenuItem primaryText="Help" />
+              <Divider />
+              <MenuItem primaryText="Sign out" />
+            </Menu>
+          </Drawer>
+          <AppBar title='Header' onLeftIconButtonTouchTap={this.openDrawer} />
           <Card />
         </div>
       </MuiThemeProvider>
