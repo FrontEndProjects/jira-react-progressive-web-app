40478da266f48f0ce4d51066f8004074013b1b23
Filip Bialek <bialek.f@gmail.com>
2016-10-24 00:57:52 +0200

    axios has new home, fighting with cors issue

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import getIssues from '../axios/getIssues';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LinearProgress from 'material-ui/LinearProgress';
@@ -42,38 +42,7 @@ export default class MainContainer extends Component {
       progress: true
     });
 
-    let login = this.state.login;
-    let password = this.state.password;
-
-    return axios({
-      url: `https://jira.nitro-digital.com/rest/api/2/search?jql=status%20in%20(Open%2C%20%22In%20Progress%22)%20AND%20assignee%20in%20(${login})`,
-      method: 'get',
-      responseType: 'json',
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      auth: {
-        username: `${login}`,
-        password: `${password}`
-      }
-    })
-    .then(response => {
-      console.log(response);
-      this.setState({
-        progress: false,
-        isLogged: true,
-        data: response.data.issues
-      });
-      console.log(this.state.data);
-    })
-    .catch(error => {
-      console.log(error);
-      let status = error.response.status === 401 ? 'Incorrect data' : '';
-      this.setState({
-        errorText: `${status}`,
-        progress: false
-      });
-    });
+    getIssues(this.state.login, this.state.password, this);
   }
 
   render () {
